b20f1f827ba260166a129efc9a2f68ac4fdb3560
Joel Ferreira <joel_ferrer_94@hotmail.com>
2024-08-18 18:08:35 -0400

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const Form = ({addColor}) => {
+  const [color, setColor] = useState("#189cb4");
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      addColor(color);
+  };
+
+  
+  return (
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input type="color" value={color} onChange={(e) => setColor(e.target.value)}/>
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#189cb4"
+        />
+        <button className="btn" type="submit" style={{ background: color }}>
+          submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
